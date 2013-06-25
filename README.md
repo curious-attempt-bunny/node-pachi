91a6b66f650fd4c94f8e3bbe689aa20cfa8e5574
Merlyn Albery-Speyer <merlyn@newrelic.com>
2013-06-25 08:18:31 -0700

    Ported from node-fuego project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Description [![build status](https://secure.travis-ci.org/curious-attempt-bunny/pachi.png)](http://next.travis-ci.org/curious-attempt-bunny/pachi)
+
+A nodejs Go Text Protocol wrapper for the pachi Go engine
+
+# Usage
+
+    var pachi = require('pachi');
+    pachi(...);
+
+# Installation
+
+    npm install pachi
+
+# License
+
+MIT
---
+++ index.js
@@ -0,0 +1,47 @@
+var spawn = require('child_process').spawn;
+var path  = require('path');
+
+module.exports = function(options) {
+  var gtp = spawn(path.join(__dirname, 'pachi-805abb4', 'pachi'));
+  var callback = null;
+  var debug = options && options.debug;
+
+  gtp.stdout.on('data', function (data) {
+    if (debug) {
+      console.log(data);
+    }
+    var response = /= ((?:.|\n)*)/g.exec(data);
+    if (callback && response) {
+      callback(null, response[1].trim());
+    } else {
+      var error = /\? ((?:.|\n)*)/g.exec(data);
+      if (callback && error) {
+        callback(error, null);
+      } else {
+        console.error("Unexpected stdout: "+data);
+      }
+    }
+  });
+
+  gtp.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+
+  gtp.on('close', function (code) {
+    console.log('child process exited with code ' + code);
+  }); 
+
+  return {
+    send: function(command, next) {
+      callback = next;
+      gtp.stdin.write(command+"\n");
+    },
+  
+    exit: function(command, next) {
+      gtp.stdin.end();
+      if (next) {
+        next();
+      }
+    }
+  };
+};
