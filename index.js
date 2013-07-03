8e00ef10ae1180f67a8baa631f86ca24cf0d7735
Merlyn Albery-Speyer <merlyn@newrelic.com>
2013-07-02 20:30:50 -0700

    Added more options

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ var spawn = require('child_process').spawn;
 var path  = require('path');
 
 module.exports = function(options) {
-  var gtp = spawn(path.join(__dirname, 'pachi-805abb4', 'pachi'));
+  var args = [];
+  if (options && options.book) {
+    args.push('-f');
+    args.push(path.join(__dirname, 'pachi-805abb4', 'book.dat'));
+  }
+  args.push('-t');
+  args.push('='+((options && options.playouts) || 2000)); 
+  var extendedOptions = [];
+  if (!(options && options.pondering == false)) {
+    extendedOptions.push('pondering=0');
+  }
+  if (options && options.threads) {
+    extendedOptions.push('threads='+options.threads);
+  }
+  if (!(options && options.maximize_score == false)) {
+    extendedOptions.push('maximize_score');
+  }
+  args.push(extendedOptions.join(','));
+
+  var gtp = spawn(path.join(__dirname, 'pachi-805abb4', 'pachi'), args);
   var callback = null;
   var debug = options && options.debug;
 
